6f2b4165a6613d8649217ae6426c9f5a1cd3f613
duanchenjie <duanchenjie@qq.com>
2020-09-23 15:24:23 +0800

    add（swagger）

--- spring-security/spring-browser/src/main/java/com/dcj/security/config/BrowserSecurityConfig.java
+++ spring-security/spring-browser/src/main/java/com/dcj/security/config/BrowserSecurityConfig.java
@@ -1,4 +1,4 @@
-package config;
+package com.dcj.security.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
